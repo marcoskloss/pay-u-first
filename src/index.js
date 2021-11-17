8a5e56a866df7794c756f7bf71b9e1af67dfd93b
marcoskloss <marcoskloss2001@gmail.com>
2021-11-16 22:31:43 -0300

    feat: users route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import Router from '@koa/router'
+import { router } from './routes'
 
 const app = new Koa()
-const router = new Router()
-
-router.get('/', ctx => {
-    ctx.body = 'hello world'
-})
 
 app.use(bodyParser())
 app.use(router.routes())
---
+++ src/modules/users/index.js
@@ -0,0 +1,15 @@
+export const list = ctx => {
+    ctx.body = 'list'
+}
+
+export const create = ctx => {
+    ctx.body = 'create'
+}
+
+export const update = ctx => {
+    ctx.body = 'update'
+}
+
+export const remove = ctx => {
+    ctx.body = 'remove'
+}
