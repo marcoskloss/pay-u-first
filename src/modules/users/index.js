9abb22c10e1e7b39c82fa9c91450cf665e11344a
marcoskloss <marcoskloss2001@gmail.com>
2021-11-25 22:30:31 -0300

    feat: login with email and password

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,9 +1,39 @@
-export const list = ctx => {
-    ctx.body = 'list'
+import { prisma } from '~/data'
+import jwt from 'jsonwebtoken'
+
+export const login = async ctx => {
+    try {
+        const { email, password } = ctx.request.body
+        const [user] = await prisma.user.findMany({
+            where: { email, password },
+        })
+
+        if (!user) {
+            ctx.status = 404
+            return
+        }
+
+        const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET)
+
+        ctx.body = { user, token }
+    } catch (error) {
+        ctx.body = 'Ops! Algo de errado aconteceu!'
+        ctx.status = 500
+    }
 }
 
-export const create = ctx => {
-    ctx.body = 'create'
+export const list = async ctx => {
+    const users = await prisma.user.findMany()
+    ctx.body = users
+}
+
+export const create = async ctx => {
+    const user = await prisma.user.create({
+        data: {
+            ...ctx.request.body,
+        },
+    })
+    ctx.body = user
 }
 
 export const update = ctx => {
