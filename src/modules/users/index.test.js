fe06801b60a5b74f034c5b4c7d12167f718cf273
marcoskloss <marcoskloss2001@gmail.com>
2021-11-30 22:09:30 -0300

    feat: test decode credentials from Basic auth token

---
+++ src/modules/users/index.test.js
@@ -0,0 +1,56 @@
+import { decodeBasicToken } from './'
+
+describe('User module', () => {
+    it('should return credentials by basic authentication token', () => {
+        const email = 'myemail@mail.com'
+        const password = 'my-secret-pass'
+        const token = Buffer.from(`${email}:${password}`).toString('base64')
+
+        const basicToken = `Basic ${token}`
+
+        const result = decodeBasicToken(basicToken)
+        expect(result).toEqual([email, password])
+    })
+
+    it('should throw new error when token is not Basic type', () => {
+        const email = 'myemail@mail.com'
+        const password = 'my-secret-pass'
+        const token = Buffer.from(`${email}:${password}`).toString('base64')
+
+        const basicToken = `Bearer ${token}`
+
+        const result = () => decodeBasicToken(basicToken)
+        expect(result).toThrowError('Wrong token type')
+    })
+
+    it('should throw new error when token is in wrong format', () => {
+        const email = 'myemail@mail.com'
+        const password = 'my-secret-pass'
+        const token = Buffer.from(`${email}${password}`).toString('base64')
+
+        const basicToken = `Basic ${token}`
+
+        const result = () => decodeBasicToken(basicToken)
+        expect(result).toThrowError('Wrong credentials format')
+    })
+
+    it('should throw new error when password is not provided', () => {
+        const email = 'myemail@mail.com'
+        const token = Buffer.from(`${email}:`).toString('base64')
+
+        const basicToken = `Basic ${token}`
+
+        const result = () => decodeBasicToken(basicToken)
+        expect(result).toThrowError('Password is not provided')
+    })
+
+    it('should throw new error when password is not provided', () => {
+        const password = 'my-secret-pass'
+        const token = Buffer.from(`:${password}`).toString('base64')
+
+        const basicToken = `Basic ${token}`
+
+        const result = () => decodeBasicToken(basicToken)
+        expect(result).toThrowError('Email is not provided')
+    })
+})
