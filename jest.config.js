e08e045b3fd7a57125f3c8b809bd3d04dd70015a
marcoskloss <marcoskloss2001@gmail.com>
2021-12-12 21:53:37 -0300

    test: change DB_URL var to avoid using same url of production

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+require('dotenv-safe/config')
+const { exec } = require('child_process')
+
+process.env.DB_URL = `${process.env.DB_URL}_testdb?schema=test_schema`
+exec('yarn db:migrate')
+
+module.exports = {}
--- src/routes.test.js
+++ src/routes.test.js
@@ -1,9 +1,17 @@
 import request from 'supertest'
+import bcrypt from 'bcrypt'
+
+import { prisma } from '~/data'
+
 import { app } from './serverSetup'
 
 const server = app.listen()
 
 describe('User routes', () => {
+    beforeEach(async () => {
+        await prisma.user.deleteMany({})
+    })
+
     it('given wrong email it should return not found', async () => {
         const email = 'useremail@mai.com'
         const password = '123456'
@@ -23,5 +31,22 @@ describe('User routes', () => {
             .auth(email, password)
 
         expect(response.status).toBe(404)
+    })
+
+    it('should return logged in user by correct credentials', async () => {
+        const email = 'marcos@email.com'
+        const password = '123456'
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        await prisma.user.create({
+            data: { email, password: hashedPassword },
+        })
+
+        const result = await request(server).get('/login').auth(email, password)
+
+        expect(result.status).toBe(200)
+        expect(result.body.user).toBeTruthy()
+        expect(result.body.user.id).toBeTruthy()
+        expect(result.body.user.email).toBeTruthy()
     })
 })
