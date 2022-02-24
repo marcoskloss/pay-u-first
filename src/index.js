6e8741684b98cbcca4d2547a181d9d0e145c7bff
Marcos Kloss <marcoskloss2001@gmail.com>
2022-02-23 21:58:02 -0300

    chore: add log when server starts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { app } from './serverSetup'
 
-app.listen(process.env.SERVER_PORT)
+app.listen(process.env.SERVER_PORT, () => {
+    console.log('server is running at', process.env.SERVER_PORT)
+})
