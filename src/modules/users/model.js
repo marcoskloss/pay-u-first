79286c7a7ebf3edd371908af36aef77a1781868d
Marcos Kloss <marcoskloss2001@gmail.com>
2022-03-22 19:54:21 -0300

    refactor: remove `User.findUnique` middleware

--- src/modules/users/model.js
+++ src/modules/users/model.js
@@ -1,14 +1,12 @@
 import { prisma } from '~/data'
 import bcrypt from 'bcrypt'
 
-const passwordCheck = async (params, next) => {
-    const { password: passwordPlainText, ...where } = params.args.where
+async function findUnique(params) {
+    const { password: passwordPlainText, ...where } = params.where
 
-    params.args.where = where
+    const result = await prisma.user.findUnique({ ...params, where })
 
-    const result = await next(params, next)
-
-    if (!result) {
+    if (!result || !passwordPlainText) {
         return result
     }
 
@@ -17,31 +15,17 @@ const passwordCheck = async (params, next) => {
         result.password
     )
 
-    if (!passwordMatch) return false
+    if (!passwordMatch) {
+        return false
+    }
 
-    return result
+    const { password: _, ...user } = result
+
+    return user
 }
 
-prisma.$use(async (params, next) => {
-    if (params.model !== 'User' || params.action !== 'findUnique') {
-        return next(params, next)
-    }
-
-    const result = params.args.where.password
-        ? await passwordCheck(params, next)
-        : await next(params, next)
-
-    if (result) {
-        // eslint-disable-next-line no-unused-vars
-        const { password: _, ...user } = result
-        return user
-    }
-
-    return result
-})
-
 export const User = {
-    findUnique: prisma.user.findUnique,
+    findUnique,
     findMany: prisma.user.findMany,
     create: prisma.user.create,
     delete: prisma.user.delete,
